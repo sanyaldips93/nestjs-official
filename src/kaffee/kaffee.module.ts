bc3c4f578ed9df0888577f83388dd62b6e5ee568
Dipayan <sanyal.dipayan@yahoo.co.in>
2021-06-18 12:24:39 +0530

    Added validation and config mods

--- src/kaffee/kaffee.module.ts
+++ src/kaffee/kaffee.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Entity } from 'typeorm';
+import { Event } from 'src/events/entities/event.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Kaffee } from './entities/kaffee.entity';
 import { KaffeeController } from './kaffee.controller';
 import { KaffeeService } from './kaffee.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Kaffee, Flavor, Entity])],
+  imports: [TypeOrmModule.forFeature([Kaffee, Flavor, Event]), ConfigModule],
   controllers: [KaffeeController],
   providers: [KaffeeService],
 })
--- src/kaffee/kaffee.service.ts
+++ src/kaffee/kaffee.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Event } from 'src/events/entities/event.entity';
@@ -16,6 +17,7 @@ export class KaffeeService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
     private readonly connection: Connection,
+    private readonly configService: ConfigService,
   ) {}
 
   findAll(paginationQueryDto: PaginationQueryDto) {
